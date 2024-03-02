73d66e93f4386395b7299eed55465bbfaf4fb65f
Tanmay Gupta <tanmay8.mitmpl2022@learner.manipal.edu>
2024-03-02 20:02:26 +0530

    Update additional.sql

--- lab4/additional.sql
+++ lab4/additional.sql
@@ -26,5 +26,34 @@ select shipment.shipdate, count(order#)
   3  group by shipdate
   4  order by count(order#) desc;
 
---Demonstrate the handling of data in ORDER_ITEMS relation on deletion of any item from the ITEM relation
+--List the order no for the orders that were shipped from all the warehouses that the company has in a specific city.
+
+ select distinct order#, warehouse.warehouse#
+  2  from shipment
+  3  join warehouse on warehouse.warehouse# = shipment.warehouse#
+  4  where warehouse.city = 'Mumbai';
+
+--List the customer with a specific surname.
+
+select *
+  2  from customer
+  3  where cname like '%Gupta%';
+
+--List the customers in descending order of their total order amount.
+
+select customer.cname, orders.ordamt
+  2  from orders
+  3  join customer on customer.cust# = orders.cust#
+  4  order by orders.ordamt desc;
+
+--Identify the customer with at least three orders that shipped on the particular date
+
+SELECT c.cust#, c.cname, COUNT(o.order#) AS order_count
+  2  FROM CUSTOMER c
+  3  JOIN ORDERS o ON c.cust# = o.cust#
+  4  JOIN SHIPMENT s ON o.order# = s.order#
+  5  WHERE s.shipdate = '18-Feb-2021'
+  6  GROUP BY c.cust#, c.cname
+  7  HAVING COUNT(o.order#) >= 3;
+
 
