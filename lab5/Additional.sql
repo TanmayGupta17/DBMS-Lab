38225b70c03223901acdc66e45297395e4edbec7
Tanmay Gupta <tanmay8.mitmpl2022@learner.manipal.edu>
2024-03-27 11:05:23 +0530

    Update Additional.sql

--- lab5/Additional.sql
+++ lab5/Additional.sql
@@ -21,16 +21,45 @@ from shipment
 
 --Select all those items which have not been bought by any customer.
 select item.item#, item.unitprice
-  2  from item
-  3  where item.item# not in (
-  4  select item#
-  5  from order_items
-  6  );
+from item
+where item.item# not in (
+select item#
+from order_items
+);
 
 --Find the item which has been bought by most of the customers.
+select item#
+from (
+select order_items.item#, count(distinct orders.cust#) as noofcustomer
+from orders
+join order_items on order_items.order# = orders.order#
+group by order_items.item#
+order by noofcustomer desc
+)
+where rownum = 1;
 
 --List the orders which are not shipped on a particular date.
+select orders.order#
+from orders
+join shipment on shipment.order# = orders.order#
+where shipment.shipdate is null or shipment.shipdate != '10-Jan-2020';
+
 --Identify the customers whose orders are shipped on a both specified days.
 --Give the customer information whose all orders are shipped from a single warehouse.
---List the customers with maximum number of orders.
+select customer.cust#, customer.cname
+from customer
+join orders on orders.cust# = customer.cust#
+join shipment on shipment.order# = orders.order#
+group by customer.cust#, customer.cname
+having count(distinct shipment.warehouse#)=1;
 
+--List the customers with maximum number of orders.
+select cust#
+from (
+select customer.cust#, count(distinct orders.order#) as nooforders
+from customer
+join orders on orders.cust# = customer.cust#
+group by customer.cust#
+order by nooforders desc
+)
+where rownum = 1;
