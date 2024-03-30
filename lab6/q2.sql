c848fc1ad13eff2232d0ebbaf815a7460e2cfc18
Tanmay Gupta <tanmay8.mitmpl2022@learner.manipal.edu>
2024-03-30 13:12:12 +0530

    Create q2.sql

---
+++ lab6/q2.sql
@@ -0,0 +1,22 @@
+set serveroutput on
+
+create or replace trigger driver_info
+after insert or update on participated
+for each row
+
+declare
+d_name varchar(20);
+d_address varchar(50);
+d_id varchar(30); 
+
+begin
+select name, address, driver_id
+into d_name, d_address, d_id
+from person natural join participated;
+
+dbms_output.put_line('Driver name: ' || d_name);
+dbms_output.put_line('Driver address: ' || d_address);
+dbms_output.put_line('Driver id: ' || d_id);
+
+end;
+/
